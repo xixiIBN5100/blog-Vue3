2cab6255e3a4b2ea126b0e892692cab1e7d72133
xixiibn5100 <1584914306@qq.com>
2024-12-11 16:45:53 +0800

    feat: add postBlog page

--- src/views/Home/PostBlog.vue
+++ src/views/Home/PostBlog.vue
@@ -1,16 +1,38 @@
-<script setup lang="ts">
-
-import TitleBar from "@/components/titleBar.vue";
-import {Document} from "@element-plus/icons-vue";
-</script>
 
 <template>
   <div style=" height: 100vh;" >
     <TitleBar>
       <el-icon><document /></el-icon> <span>发布博客</span>
     </TitleBar>
+
+    <div style="padding: 40px;margin-top: 30px">
+      <el-form>
+        <el-form-item label="博客标题" >
+          <el-input placeholder="请输入标题" v-model="submitForm.title" />
+        </el-form-item>
+        <el-form-item label="博客内容">
+          <el-input type="textarea" placeholder="请输入内容" v-model="submitForm.content" maxlength="1000" show-word-limit :rows="30" resize="none" />
+        </el-form-item>
+        <el-form-item>
+          <div style="display: flex; justify-content: center;width: 100%">
+            <el-button type="success" style="width: 100%">发布</el-button>
+          </div>
+        </el-form-item>
+      </el-form>
+    </div>
   </div>
 </template>
+<script setup lang="ts">
+
+import TitleBar from "@/components/titleBar.vue";
+import {Document} from "@element-plus/icons-vue";
+import {ref} from "vue";
+
+const submitForm = ref({
+  title: "",
+  content: ""
+})
+</script>
 
 <style scoped>
 
