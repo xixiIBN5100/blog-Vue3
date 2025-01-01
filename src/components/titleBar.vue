1cf6aec4b250fe6fb52918b32e2ff3a960d2a1ee
xixiibn5100 <1584914306@qq.com>
2025-01-01 22:21:59 +0800

    fix replay function

---
+++ src/components/titleBar.vue
@@ -0,0 +1,13 @@
+<template>
+    <div style="padding: 25px 35px;font-size: 1.1rem;display: flex;align-items: center;gap: 6px;box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);">
+      <slot/>
+    </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>
